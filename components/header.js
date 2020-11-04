a208e84f923b1d48f88fa179bfc794b606722b59
Evan.Burbidge <burbidge@tcd.ie>
2020-11-04 23:04:49 +0000

    updating headers

--- components/header.js
+++ components/header.js
@@ -2,11 +2,12 @@ import navStyles from './header.module.css'
 import HeaderListItem from './headerListItem';
 
 export default function Header() {
-  const scrolled = false;
-  const classNames = "scrolled main-nav fixed flex w-full bg-white border-b items-center justify-between flex-wrap p-5 m-auto top-0 animated";
+  const scrolled = true;
+  const classNames = "main-nav fixed flex w-full bg-white border-b items-center justify-between flex-wrap p-5 m-auto top-0 animated";
+
 
   return (
-    <nav className={scrolled ? ['scrolled', classNames] : classNames}>
+    <nav className={`${navStyles.scrolled} ${classNames}`}>
       <p>logo</p>
       <ul className="flex">
         <HeaderListItem classes="mr-3" label="home" toId="home"/>
