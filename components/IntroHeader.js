250407644b9873227375cdeaad9b0a876e6c82ec
eburbidge <eburbidge@hubspot.com>
2020-11-05 17:43:47 +0000

    add bg

--- components/IntroHeader.js
+++ components/IntroHeader.js
@@ -1,15 +1,21 @@
 import { Link } from 'react-scroll'; 
 
+const bgHeader = {
+  backgroundImage: `url('mg-bg.png')`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'fixed',
+}
+
 const IntroHeader = () => (
-  <div className="section-container h-vh pt-24 px-10 clip border-b border-gray-500" id="home">
+  <div className="section-container h-vh pt-24 px-10" id="home" style={bgHeader}>
     <div className="flex justify-between align-center  items-center h-full">
-      <div classNaem="flex flex-col">
+      <div className="flex flex-col">
         <h3 className="text-3xl font-light text-primaryDark">Hello there i'm </h3>
-        <h1 className="text-7xl font-bold capitalize text-primaryDark">The <span className="text-primary">web</span> ui <span className="text-primary">guy</span></h1>
+        <h1 className="text-7xl font-bold capitalize text-primaryDark text-lead tracking-tighter leading-tight">The <span className="text-primary">web</span> ui <span className="text-primary">guy</span></h1>
         <p className="text-base text-primaryDark w-2/3">I'm a Developer with over 6 years of experience. Experienced with all stages of the development cycle for dynamic projects</p>
       </div>
       <div>
-        <h5 className="text-3xl bg-primaryDark text-white px-5 py-2 rounded-full font-thin cursor-pointer">
+        <h5 className="text-3xl bg-primaryDark text-white px-5 py-2 rounded-full font-bold text-lead tracking-tighter leading-tight cursor-pointer">
           <Link to={'about'} spy={true} smooth={true} duration={500}>
             Read More
           </Link>
--- components/header.js
+++ components/header.js
@@ -1,18 +1,43 @@
+import { useState } from 'react'
+import Image from 'next/image';
 import navStyles from './header.module.css'
 import HeaderListItem from './headerListItem';
+import { Events } from 'react-scroll';
 
 export default function Header() {
-  const classNames = "main-nav fixed flex w-full bg-white border-b items-center justify-between flex-wrap p-5 m-auto top-0 animated";
+  const classNames = "main-nav fixed flex w-full items-center justify-between flex-wrap p-5 m-auto top-0 animated";
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  if (process.browser) {
+    window.addEventListener('scroll', () => {
+      const bounding = document.querySelector('#home').getBoundingClientRect();
+      console.log(bounding);
+      if (((bounding.y * -1) - 40) >= window.innerHeight) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    });
+    
+  }
+
+  Events.scrollEvent.register("end", (...args) => {
+    if (args[0] !== 'home') {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  });
+
   return (
-    <nav className={`${navStyles.scrolled} ${classNames}`}>
-      <p>logo</p>
+    <nav className={isScrolled ? `${navStyles.scrolled} ${classNames}` : classNames}>
+      <Image width="40" height="40" src="https://www.datocms-assets.com/28175/1604596869-webuiguy.png" />
       <ul className="flex">
-        <HeaderListItem classes="mr-3" label="home" toId="home"/>
-        <HeaderListItem classes="mr-3" label="about" toId="about"/>
-        <HeaderListItem classes="mr-3" label="blogs" toId="blogs"/>
-        {/* <HeaderListItem classes="mr-3" label="clients" toId="clients"/> */}
-        <HeaderListItem classes="mr-3" label="testimonials" toId="testimonials"/>
-        <HeaderListItem classes="mr-3" label="contact" toId="footer"/>
+        <HeaderListItem classes="mr-3" label="home" toId="home" />
+        <HeaderListItem classes="mr-3" label="about" toId="about" />
+        <HeaderListItem classes="mr-3" label="blogs" toId="blogs" />
+        <HeaderListItem classes="mr-3" label="testimonials" toId="testimonials" />
+        <HeaderListItem classes="mr-3" label="contact" toId="footer" />
       </ul>
     </nav>
   );
