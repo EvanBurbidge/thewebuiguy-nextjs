55f6436e08ecf3c0e58f1839b8557f32991304d0
eburbidge <eburbidge@hubspot.com>
2020-11-06 11:46:56 +0000

    add footer

--- components/SectionHeader.js
+++ components/SectionHeader.js
@@ -2,7 +2,7 @@ const SectionHeader = ({
   title = '',
 }) => (
     <div className="flex justify-center align-center mb-6 pt-24">
-      <h1 className="text-4xl pl-10 lg:text-5xl font-bold tracking-tighter text-primaryDark leading-tight text-center capitalize">{title}</h1>
+      <h1 className="text-4xl lg:text-5xl font-bold tracking-tighter text-primaryDark leading-tight text-center capitalize">{title}</h1>
     </div>
 )
 
--- components/footer.js
+++ components/footer.js
@@ -1,32 +1,57 @@
 import HubspotForm from './HubspotForm';
+import SectionHeader from './SectionHeader';
+import Container from './container/Container';
 
 export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2" id="footer">
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <div className="pl-10  mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            <h3 className="text-4xl mb-10 lg:text-5xl text-primaryDark font-bold tracking-tighter leading-tight text-center lg:text-left">
-              Get in touch
-            </h3>
-            <HubspotForm className="pl-10 mt-10" portalId="7355413" formId="191605af-d293-453a-bbe7-0cd24422870a" target="hubspotForm" />
-          </div>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://twitter.com/thewebuiguy"
-              className="mx-3 bg-primaryDark hover:bg-white hover:text-primaryDark border border-primaryDark text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Twitter
-            </a>
-            <a
+      <Container>
+        <div className="flex flex-col">
+          <SectionHeader title="get in touch" />
+          <hr className="mb-6"/>
+          <div className="flex justify-between w-full">
+            <div className="flex flex-col">
+              <h1 className="text-2xl mb-3 lg:text-5xl text-primaryDark font-bold tracking-tighter leading-tight">Mail me</h1>
+              <p className="text-primaryDark font-light leading-tight">thewebuiguy@gmail.com</p>
+            </div>
+            <div className="flex align-center flex-grow justify-center">
+              <HubspotForm className="mt-10" portalId="7355413" formId="191605af-d293-453a-bbe7-0cd24422870a" target="hubspotForm" />
+            </div>
+          <div className="flex flex-col">
+            <h1 className="text-2xl mb-3 lg:text-5xl text-primaryDark font-bold tracking-tighter leading-tight">Social Media</h1>
+            <ul>
+              <li className="mb-3">
+                <a
+                  href="https://twitter.com/thewebuiguy"
+                  target="_blank"
+                  className="font-light text-primaryDark hover:underline"
+                >
+                  Twitter
+                </a>
+              </li>
+              <li className="mb-3">
+              <a
               href="https://github.com/EvanBurbidge"
-              className="mx-3 font-bold hover:underline"
+              target="_blank"
+              className="font-light text-primaryDark hover:underline"
             >
               GitHub
             </a>
+              </li>
+              <li className="mb-3">
+              <a
+              href="https://www.linkedin.com/in/evan-burbidge-a0823361/"
+              target="_blank"
+              className="font-light text-primaryDark hover:underline"
+            >
+              Linkedin
+            </a>
+              </li>
+            </ul>
           </div>
-        </div>
-        <div className="flex w-full">
-        </div>
+         </div>
+         </div>
+      </Container>
     </footer>
   )
 }
