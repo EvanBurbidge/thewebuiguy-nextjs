187b59239379a68e76d1704933d6fcd354b23227
eburbidge <eburbidge@hubspot.com>
2020-11-11 14:29:00 +0000

    update blogs liste

--- components/blogList.js
+++ components/blogList.js
@@ -5,10 +5,10 @@ const BlogList = ({ blogs = [] }) => {
   return (
     <div className="section-container h-auto flex flex-col pb-24" id="blogs">
       <SectionHeader title="Blogs" />
-      <div className="flex flex-wrap w-full justify-between items-center px-10 py-10">
+      <div className="flex flex-wrap w-full justify-start items-center px-10 py-10">
         {
           blogs.map(blog => (
-            <div key={blog.blogSlug} className="w-full md:w-1/4 lg:w-1/4 px-2 mb-10 md:mb-0 lg:mb-0">
+            <div key={blog.blogSlug} className="w-full md:w-1/4 lg:w-1/4 px-2 mb-10 md:mb-4 lg:mb-4">
               <BlogListItem
                 title={blog.blogTitle}
                 excerpt={blog.blogExcerpt}
